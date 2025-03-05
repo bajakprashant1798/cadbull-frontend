15768608d30592d2723044c15dafc2ebd0c72f10
bajakprashant1798 <bajakprashant1798@gmail.com>
2025-03-06 04:45:30 +0530

    updated next.congig.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: false,
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_MAIN}/:path*`,
-      },
-    ];
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: `${process.env.NEXT_PUBLIC_API_MAIN}/:path*`,
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
