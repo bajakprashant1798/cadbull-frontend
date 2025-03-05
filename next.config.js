a6652bd00e00eb4b8b3193ae42646d435f1d68d5
bajakprashant1798 <bajakprashant1798@gmail.com>
2025-03-06 04:08:49 +0530

    updated next.congig.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+// }
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://cadbull-backend.onrender.com/api/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
