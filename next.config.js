90484ed492e2ce64db999e2ad8ada4ee2e1ce2a2
bajakprashant1798 <bajakprashant1798@gmail.com>
2025-03-06 10:52:45 +0530

    updated next.congig.js appling proxy

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-}
-
-module.exports = nextConfig
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: false,
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: `${process.env.NEXT_PUBLIC_API_MAIN}/:path*`,
-//       },
-//     ];
-//   },
-// };
+// }
 
-// module.exports = nextConfig;
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://cadbull-backend.onrender.com/api/:path*', // Proxy to your backend API
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
+
