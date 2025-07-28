2a1b8931cbfd0859c76582d37b24ce244f2badfa
bajakprashant1798 <bajakprashant1798@gmail.com>
2025-07-28 14:18:13 +0530

    google adsense configuration

--- src/pages/detail/[id]/[slug].js
+++ src/pages/detail/[id]/[slug].js
@@ -859,9 +859,23 @@ const ViewDrawing = ({ initialProject, initialSimilar, canonicalUrl }) => {
 
                 {/* add component started */}
                 <div className="bg-white shadow-sm p-3 rounded-2">
-                  <AdSense slot="7742900751" format="fluid" layout="in-article" />
+                  <AdSense slot="2091281415" format="fluid" layout="in-article" />
                 </div>
                 {/* add component ended */}
+
+                {/* add component started */}
+                <div className="bg-white shadow-sm p-3 rounded-2">
+                  <AdSense slot="2091281415" />
+                </div>
+                {/* add component ended */}
+
+                {/* add component started */}
+                <div className="bg-white shadow-sm p-3 rounded-2">
+                  <AdSense slot="2091281415" />
+                </div>
+                {/* add component ended */}
+
+                
 
                 {/* <div>
                   <img
