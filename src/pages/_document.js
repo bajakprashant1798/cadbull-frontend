0bdea665a65837af332b5a72fc1723bfab226c6a
bajakprashant1798 <bajakprashant1798@gmail.com>
2025-08-03 16:03:14 +0530

    configured google analitics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        {/* ✅ Google Tag Manager (noscript) Fallback */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
