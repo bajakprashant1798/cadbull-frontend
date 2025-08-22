6779387e02b655d885697243727cde7fdaa1bbb4
bajakprashant1798 <bajakprashant1798@gmail.com>
2025-08-23 00:52:45 +0530

    fix: added right url in amp again

--- src/pages/amp/[id]/[slug].js
+++ src/pages/amp/[id]/[slug].js
@@ -546,7 +546,7 @@ export default function AmpProductPage({ product, similar, publisher, categoryNa
                 // ✅ UPDATED: Construct the image URL with the new path
                 const similarImageUrl = sim.image
                   ? `https://beta-assets.cadbull.com/product_img/original/${sim.image}`
-                  : "https://thumb.cadbull.com/img/product_img/large/default-amp.png";
+                  : "https://cadbull.com/default-img.png";
 
                 return (
                   <div
