78da500046511f5642aa033012c8c5c811f609ee
Prashant Bajak <bajakprashant1798@gmail.com>
2025-09-04 14:56:50 +0530

    new layout description proper fix

--- src/pages/detail/[id]/[slug].js
+++ src/pages/detail/[id]/[slug].js
@@ -736,12 +736,18 @@ const ViewDrawing = ({ initialProject, initialSimilar, canonicalUrl }) => {
                   <div className="row mb-3 mt-3">
                     <div className="col-md-12">
                       <div className="mb-md-3 mb-4 shadow-sm px-5 py-3 rounded-1" style={{ background: "#E9E9EB" }}>
-                        <div className="px-3">
-                          <SectionHeading
-                            mainHeading={""}
-                            subHeading={" "}
-                            mainHeadingBold={"Description"}
-                          />
+                        <div className="px-4">
+                          // <SectionHeading
+                          //   mainHeading={""}
+                          //   subHeading={" "}
+                          //   mainHeadingBold={"Description"}
+                          // />
+                           <div className={`position-relative d-inline-flex main-heading-wrapper-product `}>
+                            <div>
+                              <h4 className="d-inline-block mb-3 h6" style={{ minHeight: 6 }}></h4>
+                              <h5>{Description}</h5>
+                            </div>
+                          </div>
                           <div>{parse(`${project.description}`)}</div>
                           
                         </div>
