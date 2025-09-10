cefa50a9a89b1cb155cd1d5519cbb960e56269e2
bajakprashant1798 <bajakprashant1798@gmail.com>
2025-09-10 23:30:43 +0530

    Fix14: login and register registerPhone login fix

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,17 +2,7 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, RecaptchaVerifier } from "firebase/auth";
 
-// Only initialize ONCE!
-// const app = !getApps().length
-//   ? initializeApp({
-//       apiKey: "AIzaSyB1aPF5DktAc6yojjYODVWA7jVUtf-UqTU",
-//       authDomain: "cadbull-8e9a8.firebaseapp.com",
-//       projectId: "cadbull-8e9a8",
-//       storageBucket: "cadbull-8e9a8.appspot.com",
-//       messagingSenderId: "279271034408",
-//       appId: "1:279271034408:web:260181d0132a8459f82f1d"
-//     })
-//   : getApps()[0];
+
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
