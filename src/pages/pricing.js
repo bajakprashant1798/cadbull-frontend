09d41a1ecafad6af7c206a00a098d9a4f8bb8656
bajakprashant1798 <bajakprashant1798@gmail.com>
2026-05-01 13:34:53 +0530

    test new price

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -248,7 +248,7 @@ const Pricing = () => {
 
                       return
                     }
-                    handleSubscription("price_1RjwuyFy6VKViPpJHrCcff1Y", user.id).then((res) => {
+                    handleSubscription("price_1TSC37Fy6VKViPpJdmLBSvfx", user.id).then((res) => {
                       // console.log("API Response:", res.data); // Log response
                       window.location.href = res.data.url
                     }).catch((err) => {
